443a71ea91680ea5bd7f28f47f650fb68057ed02
chuke <ken.0416.leo@gmail.com>
2022-03-01 02:17:48 +0900

    add first code

---
+++ MqttServerApp/MqttServerApp/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿
+namespace MqttServerApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.logPanel = new System.Windows.Forms.Panel();
+            this.informationPanel = new System.Windows.Forms.Panel();
+            this.logTextBox = new System.Windows.Forms.TextBox();
+            this.urlLabel = new System.Windows.Forms.Label();
+            this.connectionStatusLabel = new System.Windows.Forms.Label();
+            this.currentConnectionStatusLabel = new System.Windows.Forms.Label();
+            this.startServerButton = new System.Windows.Forms.Button();
+            this.logPanel.SuspendLayout();
+            this.informationPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // logPanel
+            // 
+            this.logPanel.Controls.Add(this.logTextBox);
+            this.logPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.logPanel.Location = new System.Drawing.Point(0, 105);
+            this.logPanel.Name = "logPanel";
+            this.logPanel.Size = new System.Drawing.Size(800, 345);
+            this.logPanel.TabIndex = 0;
+            // 
+            // informationPanel
+            // 
+            this.informationPanel.Controls.Add(this.startServerButton);
+            this.informationPanel.Controls.Add(this.currentConnectionStatusLabel);
+            this.informationPanel.Controls.Add(this.connectionStatusLabel);
+            this.informationPanel.Controls.Add(this.urlLabel);
+            this.informationPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.informationPanel.Location = new System.Drawing.Point(0, 0);
+            this.informationPanel.Name = "informationPanel";
+            this.informationPanel.Size = new System.Drawing.Size(800, 105);
+            this.informationPanel.TabIndex = 1;
+            // 
+            // logTextBox
+            // 
+            this.logTextBox.BackColor = System.Drawing.Color.White;
+            this.logTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.logTextBox.ForeColor = System.Drawing.Color.Black;
+            this.logTextBox.Location = new System.Drawing.Point(0, 0);
+            this.logTextBox.Multiline = true;
+            this.logTextBox.Name = "logTextBox";
+            this.logTextBox.ReadOnly = true;
+            this.logTextBox.Size = new System.Drawing.Size(800, 345);
+            this.logTextBox.TabIndex = 0;
+            // 
+            // urlLabel
+            // 
+            this.urlLabel.AutoSize = true;
+            this.urlLabel.Location = new System.Drawing.Point(44, 24);
+            this.urlLabel.Name = "urlLabel";
+            this.urlLabel.Size = new System.Drawing.Size(27, 12);
+            this.urlLabel.TabIndex = 0;
+            this.urlLabel.Text = "URL";
+            // 
+            // connectionStatusLabel
+            // 
+            this.connectionStatusLabel.AutoSize = true;
+            this.connectionStatusLabel.Location = new System.Drawing.Point(44, 53);
+            this.connectionStatusLabel.Name = "connectionStatusLabel";
+            this.connectionStatusLabel.Size = new System.Drawing.Size(99, 12);
+            this.connectionStatusLabel.TabIndex = 1;
+            this.connectionStatusLabel.Text = "Connection Status";
+            // 
+            // currentConnectionStatusLabel
+            // 
+            this.currentConnectionStatusLabel.AutoSize = true;
+            this.currentConnectionStatusLabel.Location = new System.Drawing.Point(209, 53);
+            this.currentConnectionStatusLabel.Name = "currentConnectionStatusLabel";
+            this.currentConnectionStatusLabel.Size = new System.Drawing.Size(46, 12);
+            this.currentConnectionStatusLabel.TabIndex = 2;
+            this.currentConnectionStatusLabel.Text = "Stopped";
+            // 
+            // startServerButton
+            // 
+            this.startServerButton.Location = new System.Drawing.Point(530, 53);
+            this.startServerButton.Name = "startServerButton";
+            this.startServerButton.Size = new System.Drawing.Size(75, 23);
+            this.startServerButton.TabIndex = 3;
+            this.startServerButton.Text = "Start Server";
+            this.startServerButton.UseVisualStyleBackColor = true;
+            this.startServerButton.Click += new System.EventHandler(this.startServerButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.informationPanel);
+            this.Controls.Add(this.logPanel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.logPanel.ResumeLayout(false);
+            this.logPanel.PerformLayout();
+            this.informationPanel.ResumeLayout(false);
+            this.informationPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel logPanel;
+        private System.Windows.Forms.TextBox logTextBox;
+        private System.Windows.Forms.Panel informationPanel;
+        private System.Windows.Forms.Label urlLabel;
+        private System.Windows.Forms.Label connectionStatusLabel;
+        private System.Windows.Forms.Label currentConnectionStatusLabel;
+        private System.Windows.Forms.Button startServerButton;
+    }
+}
+
---
+++ MqttServerApp/MqttServerApp/Form1.cs
@@ -0,0 +1,75 @@
+﻿using MQTTnet;
+using MQTTnet.Server;
+using System;
+using System.Windows.Forms;
+
+namespace MqttServerApp
+{
+    public partial class Form1 : Form
+    {
+        private MqttServer mqttServer = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void startServerButton_Click(object sender, EventArgs e)
+        {
+            StartServer();
+        }
+
+        private async void StartServer()
+        {
+            var optionsBuilder = new MqttServerOptionsBuilder()
+                .WithDefaultEndpoint().WithDefaultEndpointPort(1883).WithSubscriptionInterceptor(c =>
+                {
+                    c.AcceptSubscription = true;
+                })
+                .WithApplicationMessageInterceptor(c =>
+                {
+                    c.AcceptPublish = true;
+                });
+
+            mqttServer = new MqttFactory().CreateMqttServer() as MqttServer;
+            
+            mqttServer.StartedHandler = new MqttServerStartedHandlerDelegate(new Action<EventArgs>(e =>
+            {
+                this.currentConnectionStatusLabel.BeginInvoke(new Action(() =>
+                {
+                    this.currentConnectionStatusLabel.Text = "Disconnected";
+                }));
+            }));
+
+            mqttServer.StoppedHandler = new MqttServerStoppedHandlerDelegate(new Action<EventArgs>(e =>
+            {
+                this.currentConnectionStatusLabel.BeginInvoke(new Action(() =>
+                {
+                    this.currentConnectionStatusLabel.Text = "Stopped";
+                }));
+            }));
+
+            mqttServer.ClientConnectedHandler = new MqttServerClientConnectedHandlerDelegate(new Action<EventArgs>(e =>
+            {
+                this.currentConnectionStatusLabel.BeginInvoke(new Action(() => 
+                {
+                    this.currentConnectionStatusLabel.Text = "Connected";
+                }));
+            }));
+
+            mqttServer.ClientDisconnectedHandler = new MqttServerClientDisconnectedHandlerDelegate(new Action<EventArgs>(e =>
+            {
+                this.currentConnectionStatusLabel.BeginInvoke(new Action(() =>
+                {
+                    this.currentConnectionStatusLabel.Text = "Disconnected";
+                }));
+            }));
+
+            await mqttServer.StartAsync(optionsBuilder.Build());
+            this.logTextBox.BeginInvoke(new Action(() =>
+            {
+                this.logTextBox.Text += "MQTT Server is started." + Environment.NewLine;
+            }));
+        }
+    }
+}

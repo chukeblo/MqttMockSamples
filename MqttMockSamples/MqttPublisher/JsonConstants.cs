309bd1cb13122af7d211e0ec0f5dcc7a9cad6db9
chuke <ken.0416.leo@gmail.com>
2022-03-05 17:08:43 +0900

    add dummy constant definitions into mqtt publisher project

---
+++ MqttMockSamples/MqttPublisher/JsonConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace MqttPublisher
+{
+    static class JsonConstants
+    {
+        public const string TimeKey = "time";
+        public const string TemperatureKey = "temperature";
+        public const string HumidityKey = "humidity";
+    }
+}
---
+++ MqttMockSamples/MqttPublisher/MqttConstants.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MqttPublisher
+{
+    static class MqttConstants
+    {
+        // client info
+        public const string ClientId = "PublisherId";
+        public const int Port = 8883;
+        public const string Host = "localhost";
+        // certificates related info
+        public const string CertsDirPath = @"C:\hoge\";
+        public const string DeviceCertificateFileName = "xxxx-certificate.pen.crt";
+        public const string PrivateKeyFileName = "xxxx-private.pem.crt";
+        public const string RootCAFileName = "AmazonRootCA1.pem";
+    }
+}

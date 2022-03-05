c35503575520fc6eaa0de3a582fc8ceb5e556de7
chuke <ken.0416.leo@gmail.com>
2022-03-05 17:11:01 +0900

    add tls connecting operations to mqtt publisher project

--- MqttMockSamples/MqttPublisher/Program.cs
+++ MqttMockSamples/MqttPublisher/Program.cs
@@ -1,8 +1,15 @@
 ﻿using MQTTnet;
 using MQTTnet.Client;
 using MQTTnet.Client.Options;
+using OpenSSL.X509Certificate2Provider;
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Security;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Text.Json;
 using System.Threading;
 
 namespace MqttPublisher
@@ -12,17 +19,49 @@ namespace MqttPublisher
         private static IMqttClient client;
         private static IMqttClientOptions options;
 
+        private static RootCATrust rootCATrust;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Starting Publisher...");
             try
             {
-                client = new MqttFactory().CreateMqttClient();
+                var mqttFactory = new MqttFactory();
+
+                var deviceCertificateString = File.ReadAllText(
+                    MqttConstants.CertsDirPath + MqttConstants.DeviceCertificateFileName);
+                var privateKeyString = File.ReadAllText(
+                    MqttConstants.CertsDirPath + MqttConstants.PrivateKeyFileName);
+                var rootCAString = File.ReadAllText(
+                    MqttConstants.CertsDirPath + MqttConstants.RootCAFileName);
+
+                ICertificateProvider certProvider = new CertificateFromFileProvider(
+                    deviceCertificateString, privateKeyString, true);
+                var deviceCertificate = certProvider.Certificate;
+
+                var certBytes = Encoding.UTF8.GetBytes(rootCAString);
+                var signingCert = new X509Certificate2(certBytes);
+
+                rootCATrust = new RootCATrust();
+                rootCATrust.AddCert(signingCert);
+
+                var certs = new List<X509Certificate>
+                {
+                    signingCert, deviceCertificate
+                };
+
+                var tlsOptions = new MqttClientOptionsBuilderTlsParameters();
+                tlsOptions.Certificates = certs;
+                tlsOptions.SslProtocol = SslProtocols.Tls12;
+                tlsOptions.UseTls = true;
+                tlsOptions.AllowUntrustedCertificates = true;
+                tlsOptions.CertificateValidationHandler += rootCATrust.VerifyServerCertificate;
+
+                client = mqttFactory.CreateMqttClient();
                 options = new MqttClientOptionsBuilder()
-                    .WithClientId("PublisherId")
-                    .WithTcpServer("localhost", 1884)
-                    .WithCredentials("bud", "%spenser%")
-                    .WithCleanSession()
+                    .WithClientId(MqttConstants.ClientId)
+                    .WithTcpServer(MqttConstants.Host, MqttConstants.Port)
+                    .WithTls(tlsOptions)
                     .Build();
 
                 client.UseConnectedHandler(e =>
@@ -63,22 +102,102 @@ namespace MqttPublisher
 
         private static void SimulatePublish()
         {
-            var counter = 0;
-            while (counter++ < 10)
+            var floatValues = new List<float>
             {
-                var testMessage = new MqttApplicationMessageBuilder()
+                10.34f, 12.53f, 24.54f, 18.88f, 21.54f
+            };
+
+            var jsonDictionaries = new List<Dictionary<string, object>>();
+            foreach (var val in floatValues)
+            {
+                var jsonDictionary = new Dictionary<string, object>
+                {
+                    [JsonConstants.TimeKey] = DateTime.Now.ToString(),
+                    [JsonConstants.TemperatureKey] = val.ToString(),
+                    [JsonConstants.HumidityKey] = (val + 2.0f).ToString()
+                };
+                jsonDictionaries.Add(jsonDictionary);
+            }
+
+            var messages = new List<string>();
+            foreach (var dic in jsonDictionaries)
+            {
+                var json = JsonSerializer.Serialize(dic);
+                var message = new MqttApplicationMessageBuilder()
                     .WithTopic("test")
-                    .WithPayload($"Payload = {counter}")
+                    .WithPayload(json)
                     .WithExactlyOnceQoS()
                     .WithRetainFlag()
                     .Build();
-
                 if (client.IsConnected)
                 {
-                    Console.WriteLine($"publishing at {DateTime.UtcNow}");
-                    client.PublishAsync(testMessage);
+                    Console.WriteLine($"publishing message. payload = {json}");
+                    client.PublishAsync(message);
+                    Thread.Sleep(1000);
                 }
-                Thread.Sleep(2000);
+            }
+        }
+
+        internal class RootCATrust
+        {
+            X509Certificate2Collection certificates;
+            internal RootCATrust()
+            {
+                certificates = new X509Certificate2Collection();
+            }
+
+            internal void AddCert(X509Certificate2 certificate)
+            {
+                certificates.Add(certificate);
+            }
+
+            internal bool VerifyServerCertificate(MqttClientCertificateValidationCallbackContext context)
+                => VerifyServerCertificate(new object(), context.Certificate, context.Chain, context.SslPolicyErrors);
+
+            internal bool VerifyServerCertificate(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+            {
+                if (sslPolicyErrors == SslPolicyErrors.None)
+                {
+                    return true;
+                }
+                var newChain = new X509Chain();
+                var testChain = chain;
+
+                testChain.ChainPolicy.ExtraStore.AddRange(certificates);
+                testChain.ChainPolicy.VerificationFlags = X509VerificationFlags.NoFlag;
+                testChain.ChainPolicy.RevocationMode = X509RevocationMode.NoCheck;
+
+                var buildResult = testChain.Build(new X509Certificate2(certificate));
+
+                if (buildResult)
+                {
+                    return true;
+                }
+
+                foreach (var status in testChain.ChainStatus)
+                {
+                    if (status.Status != X509ChainStatusFlags.UntrustedRoot)
+                    {
+                        return false;
+                    }
+                }
+
+                foreach (var chainElement in testChain.ChainElements)
+                {
+                    foreach (var chainStatus in chainElement.ChainElementStatus)
+                    {
+                        if (chainStatus.Status == X509ChainStatusFlags.UntrustedRoot)
+                        {
+                            var foundCertificate = certificates.Find(X509FindType.FindByThumbprint, chainElement.Certificate.Thumbprint, false);
+                            if (foundCertificate.Count == 0)
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                }
+
+                return true;
             }
         }
     }
